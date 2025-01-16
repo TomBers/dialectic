557daeca855f7a186aee5c3e687bb622a215bb23
TomBers <tjcberman@gmail.com>
2025-01-16 16:39:19 +0000

    improve implementation

--- lib/dialectic/graph/sample.ex
+++ lib/dialectic/graph/sample.ex
@@ -4,31 +4,30 @@ defmodule Dialectic.Graph.Sample do
   # To test - Graph A -> B, A -> C, B -> D and C -> D
   def run do
     graph = :digraph.new()
-    a = %Vertex{name: "A", description: "A description"}
-    b = %Vertex{name: "Bb", description: "B description"}
-    c = %Vertex{name: "Cc", description: "C description"}
-    # d = %Vertex{name: "Dd"}
 
-    v1 = :digraph.add_vertex(graph, a)
-    v2 = :digraph.add_vertex(graph, b)
-    v3 = :digraph.add_vertex(graph, c)
-    # v4 = :digraph.add_vertex(graph, d)
+    v1 = add_node(graph, "A")
+    v2 = add_node(graph, "B")
+    v3 = add_node(graph, "C")
 
     :digraph.add_edge(graph, v1, v2)
     :digraph.add_edge(graph, v1, v3)
-    # :digraph.add_edge(graph, v2, v4)
-    # :digraph.add_edge(graph, v3, v4)
-    # IO.inspect(graph, label: "Graph")
 
     graph
   end
 
-  def add_child(graph, parent) do
-    thesis = %Vertex{name: "#{parent.name}_Thesis"}
-    theis_child = :digraph.add_vertex(graph, thesis)
+  def add_node(graph, name) do
+    add_node(graph, name, "#{name} description")
+  end
 
-    antithesis = %Vertex{name: "#{parent.name}_Antithesis"}
-    antithesis_child = :digraph.add_vertex(graph, antithesis)
+  def add_node(graph, name, description) do
+    vertex = %Vertex{name: name, description: description}
+    :digraph.add_vertex(graph, vertex)
+  end
+
+  def add_child(graph, parent) do
+    theis_child = add_node(graph, "#{parent.name}_Thesis")
+    antithesis_child = add_node(graph, "#{parent.name}_Antithesis")
+
     :digraph.add_edge(graph, parent, theis_child)
     :digraph.add_edge(graph, parent, antithesis_child)
     graph
