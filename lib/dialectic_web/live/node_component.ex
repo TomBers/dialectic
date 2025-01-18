b9d11268652e5c782ecae5440c7cacf8f4467963
TomBers <tjcberman@gmail.com>
2025-01-18 18:42:10 +0000

    tabs

--- lib/dialectic_web/live/node_component.ex
+++ lib/dialectic_web/live/node_component.ex
@@ -1,6 +1,33 @@
 defmodule DialecticWeb.NodeComponent do
   use DialecticWeb, :live_component
 
+  def update(assigns, socket) do
+    tabs = %{
+      "A" => ~H"""
+      <div>Content for Tab A</div>
+      """,
+      "B" => ~H"""
+      <div>Content for Tab B</div>
+      """,
+      "C" => ~H"""
+      <div>Content for Tab C</div>
+      """,
+      "D" => ~H"""
+      <div>Content for Tab D</div>
+      """
+    }
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(:tabs, tabs)
+     |> assign(:active_tab, "A")}
+  end
+
+  def handle_event("change_tab", %{"tab" => tab}, socket) do
+    {:noreply, assign(socket, :active_tab, tab)}
+  end
+
   def render(assigns) do
     ~H"""
     <div class="node">
@@ -8,7 +35,9 @@ defmodule DialecticWeb.NodeComponent do
       <p>
         Parent:
         <%= if Map.has_key?(@node.parent, :id) do %>
-          <a href="#" phx-click="node_clicked" phx-value-id={@node.parent.id}>{@node.parent.id}</a>
+          <a href="#" class="text-blue-600" phx-click="node_clicked" phx-value-id={@node.parent.id}>
+            {@node.parent.id}
+          </a>
         <% else %>
           None
         <% end %>
@@ -16,9 +45,32 @@ defmodule DialecticWeb.NodeComponent do
       <p>
         Children:
         <%= for child <- assigns.node.children do %>
-          <a href="#" phx-click="node_clicked" phx-value-id={child.id}>{child.id}</a>
+          <a href="#" class="text-blue-600 px-2" phx-click="node_clicked" phx-value-id={child.id}>
+            {child.id}
+          </a>
         <% end %>
       </p>
+      <nav class="flex -mb-px" role="tablist">
+        <%= for tab <- Map.keys(@tabs) do %>
+          <button
+            phx-click="change_tab"
+            phx-value-tab={tab}
+            phx-target={@myself}
+            type="button"
+            class={[
+              "py-2 px-4 border-b-2 font-medium text-sm",
+              @active_tab == tab && "border-blue-500 text-blue-600",
+              @active_tab != tab &&
+                "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+            ]}
+          >
+            {tab}
+          </button>
+        <% end %>
+      </nav>
+      <div class="mt-4">
+        {Map.get(@tabs, @active_tab)}
+      </div>
       <.form for={@form} phx-submit="save">
         <div>
           <.input field={@form[:description]} type="textarea" label="Name" />
