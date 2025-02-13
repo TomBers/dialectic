1a67c2c67a571e4ed0e1caaa6e5b0c0c958cc726
TomBers <tjcberman@gmail.com>
2025-02-13 18:26:40 +0000

    add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Dialectic.Repo
+alias Dialectic.Accounts.Graph
+alias Dialectic.Graph.Serialise
+
+# TODO - curate a list of graphs to seed
+["satre", "bob"]
+|> Enum.each(fn graph_name ->
+  data = Serialise.load_graph_as_json(graph_name)
+
+  %Graph{}
+  |> Graph.changeset(%{
+    title: graph_name,
+    user_id: nil,
+    data: data,
+    is_public: false,
+    is_deleted: false,
+    is_published: true
+  })
+  |> Repo.insert!()
+end)
