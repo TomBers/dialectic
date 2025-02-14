698dc4ce64a51d61a2cea53c6b6e91cd3de7fc5d
TomBers <tjcberman@gmail.com>
2025-02-14 10:54:36 +0000

    add seed files

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,8 +13,22 @@ alias Dialectic.Repo
 alias Dialectic.Accounts.Graph
 alias Dialectic.Graph.Serialise
 
-# TODO - curate a list of graphs to seed
-["satre", "bob"]
+[
+  "satre",
+  "bob",
+  "German Idealism",
+  "Small is Beautiful - Schumacher",
+  "The Republic - Plato",
+  "branch",
+  "What is ethics?",
+  "narcissus",
+  "What is good and what is evil?",
+  "combine",
+  "normative ethical theories",
+  "Why do we dream?",
+  "reply",
+  "answer"
+]
 |> Enum.each(fn graph_name ->
   data = Serialise.load_graph_as_json(graph_name)
 
