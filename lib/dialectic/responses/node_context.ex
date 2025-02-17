eb5063a33f9da469fa9bf2571076011eba88362a
TomBers <tjcberman@gmail.com>
2025-02-17 11:15:16 +0000

    node context module

---
+++ lib/dialectic/responses/node_context.ex
@@ -0,0 +1,7 @@
+defmodule Dialectic.Responses.NodeContext do
+  def run do
+    :ok
+    # {graph_struct, graph} = GraphManager.get_graph("Satre")
+    # node = GraphManager.find_node_by_id(graph, "2")
+  end
+end
