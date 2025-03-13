101d6866c6d37f7319359655d09bd9af13c86434
TomBers <tjcberman@gmail.com>
2025-03-13 11:13:51 +0000

    simple page added

--- lib/dialectic_web/live/linear_graph_live.ex
+++ lib/dialectic_web/live/linear_graph_live.ex
@@ -9,4 +9,19 @@ defmodule DialecticWeb.LinearGraphLive do
 
     {:ok, assign(socket, conv: conv, graph_struct: graph_struct)}
   end
+
+  defp full_html(content) do
+    Earmark.as_html!(content) |> Phoenix.HTML.raw()
+  end
+
+  defp message_border_class(class) do
+    case class do
+      # "user" -> "border-red-400"
+      "answer" -> "border-blue-400"
+      "thesis" -> "border-green-400"
+      "antithesis" -> "border-red-400"
+      "synthesis" -> "border-purple-600"
+      _ -> "border-gray-200"
+    end
+  end
 end
