2db488c93d6ae76b494fda360a83daf4087b2b25
TomBers <tjcberman@gmail.com>
2025-04-23 11:25:49 +0100

    fixing graph updates

--- assets/js/graph_hook.js
+++ assets/js/graph_hook.js
@@ -17,29 +17,49 @@ const graphHook = {
     this.cy = draw_graph(div_id, this, elements, cols, node);
   },
   updated() {
-    const { graph, node, updateview } = this.el.dataset;
+    const { graph, node } = this.el.dataset;
 
     const newElements = JSON.parse(graph);
     this.cy.json({ elements: newElements });
 
-    if (newElements.length != numNodes.length) {
-      this.cy
-        .layout({
-          name: "dagre",
-          rankDir: "TB",
-          nodeSep: 20,
-          edgeSep: 15,
-          rankSep: 30,
-        })
-        .run();
-    }
-    if (node != nodeId && updateview == "true") {
-      this.cy.animate({
-        center: {
-          eles: `#${node}`,
+    const prevNodeCount = numNodes.filter(
+      (n) => n.data && n.data.compound !== true,
+    );
+
+    const newNodeCount = newElements.filter(
+      (n) => n.data && n.data.compound !== true,
+    );
+
+    // Only relayout if the number of actual nodes changed
+    if (prevNodeCount.length !== newNodeCount.length) {
+      // Create the layout but don't run it yet
+      const layout = this.cy.layout({
+        name: "dagre",
+        rankDir: "TB",
+        nodeSep: 20,
+        edgeSep: 15,
+        rankSep: 30,
+        // Add a callback for when layout is done
+        ready: function () {
+          console.log("Layout ready");
         },
-        zoom: 2,
+        // This gets called when the layout is done running
+        stop: function () {
+          console.log("Layout finished");
+          // Now center on the node
+          this.cy.animate({
+            center: {
+              eles: `#${node}`,
+            },
+            zoom: 2,
+            // Optional: make this animation a bit slower to allow user to see context
+            duration: 750,
+          });
+        }.bind(this), // Bind 'this' to maintain context
       });
+
+      // Run the layout
+      layout.run();
     }
 
     this.cy.elements().removeClass("selected");
