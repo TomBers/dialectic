95f261525d01d3f177562b0b318876efd7c273b4
TomBers <tjcberman@gmail.com>
2025-05-03 09:41:24 +0100

    combine notes buttons

--- lib/dialectic_web/live/note_menu_comp.ex
+++ lib/dialectic_web/live/note_menu_comp.ex
@@ -35,18 +35,6 @@ defmodule DialecticWeb.NoteMenuComp do
         </h2>
       </div>
 
-      <div class="flex items-center bg-amber-50 px-2 py-0.5 rounded-full" title="Number of stars">
-        <span class="text-amber-600 mr-1">{length(@node.noted_by)}</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-3.5 w-3.5 text-amber-500"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-        </svg>
-      </div>
-
       <div class="flex items-center space-x-2">
         <%= if Enum.any?(@node.noted_by, fn u -> u == @user end) do %>
           <button
@@ -75,7 +63,7 @@ defmodule DialecticWeb.NoteMenuComp do
                 d="M6 18L18 6M6 6l12 12"
               />
             </svg>
-            Unnote
+            Unnote ({length(@node.noted_by)})
           </button>
         <% else %>
           <button
@@ -98,7 +86,7 @@ defmodule DialecticWeb.NoteMenuComp do
                 d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
               />
             </svg>
-            Note
+            Note ({length(@node.noted_by)})
           </button>
         <% end %>
 
