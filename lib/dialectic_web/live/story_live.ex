efd12621f574f956087feb8eaadd2c04f38683cc
TomBers <tjcberman@gmail.com>
2025-05-21 12:15:07 +0100

    add cycling of paths

--- lib/dialectic_web/live/story_live.ex
+++ lib/dialectic_web/live/story_live.ex
@@ -12,24 +12,48 @@ defmodule DialecticWeb.StoryLive do
       GraphManager.path_to_node(graph_id, ln)
       |> Enum.reverse()
 
-    {:ok, assign(socket, graph_id: graph_id, path: path, leaf_nodes: leaf_nodes)}
+    {:ok, assign(socket, graph_id: graph_id, path: path, leaf_nodes: leaf_nodes, p_index: 0)}
   end
 
   def handle_event("next", _, socket) do
-    ln = List.last(socket.assigns.leaf_nodes)
-    return_path(socket, ln)
+    p_indx =
+      if socket.assigns.p_index < length(socket.assigns.leaf_nodes) - 1 do
+        socket.assigns.p_index + 1
+      else
+        0
+      end
+
+    IO.inspect(p_indx, label: "P Index")
+
+    return_path(
+      socket,
+      p_indx
+    )
   end
 
   def handle_event("previous", _, socket) do
-    ln = List.first(socket.assigns.leaf_nodes)
-    return_path(socket, ln)
+    p_indx =
+      if socket.assigns.p_index > 1 do
+        socket.assigns.p_index - 1
+      else
+        length(socket.assigns.leaf_nodes) - 1
+      end
+
+    IO.inspect(p_indx, label: "P Index")
+
+    return_path(
+      socket,
+      p_indx
+    )
   end
 
-  defp return_path(socket, leaf) do
+  defp return_path(socket, p_index) do
+    leaf = Enum.at(socket.assigns.leaf_nodes, p_index)
+
     path =
       GraphManager.path_to_node(socket.assigns.graph_id, leaf)
       |> Enum.reverse()
 
-    {:noreply, assign(socket, path: path)}
+    {:noreply, assign(socket, path: path, p_index: p_index)}
   end
 end
