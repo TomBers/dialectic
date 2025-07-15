447a99a030eb8471b4080760c1161a563e93316e
TomBers <tjcberman@gmail.com>
2025-07-15 11:23:54 +0100

    add menu toggle

--- lib/dialectic_web/live/node_comp.ex
+++ lib/dialectic_web/live/node_comp.ex
@@ -20,7 +20,11 @@ defmodule DialecticWeb.NodeComp do
               "flex-grow overflow-auto pb-4 pt-4"
             ]}
             id={"tt-node-" <> @node.id}
-            style="max-height: calc(100vh - 400px);"
+            style={
+              if @menu_visible,
+                do: "max-height: calc(50vh - 100px);",
+                else: "max-height: calc(100vh - 100px);"
+            }
           >
             <div class="summary-content" id={"tt-summary-content-" <> @node.id}>
               <article class={[
@@ -52,7 +56,39 @@ defmodule DialecticWeb.NodeComp do
           </div>
         <% end %>
       </div>
-      <div class="nodeMenuComp sticky bottom-0 bg-white w-full z-10">
+      <div class="flex justify-center mb-2">
+        <button
+          phx-click="toggle_menu"
+          phx-target={@myself}
+          class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-1 px-4 rounded inline-flex items-center"
+        >
+          <span>{if @menu_visible, do: "Hide Menu", else: "Show Menu"}</span>
+          <svg
+            class="ml-2 h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <%= if @menu_visible do %>
+              <path
+                fill-rule="evenodd"
+                d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+                clip-rule="evenodd"
+              />
+            <% else %>
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            <% end %>
+          </svg>
+        </button>
+      </div>
+      <div
+        class={["nodeMenuComp sticky bottom-0 bg-white w-full z-10", if(!@menu_visible, do: "hidden")]}
+        style="transition: height 0.3s ease-in-out;"
+      >
         <.live_component
           module={NodeMenuComp}
           id="node-menu-comp"
@@ -79,7 +115,12 @@ defmodule DialecticWeb.NodeComp do
        form: Map.get(assigns, :form, nil),
        cut_off: Map.get(assigns, :cut_off, 500),
        ask_question: Map.get(assigns, :ask_question, true),
-       graph_id: Map.get(assigns, :graph_id, "")
+       graph_id: Map.get(assigns, :graph_id, ""),
+       menu_visible: true
      )}
+  end
+
+  def handle_event("toggle_menu", _, socket) do
+    {:noreply, assign(socket, menu_visible: !socket.assigns.menu_visible)}
   end
 end
