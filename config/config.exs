bd5cef45f2c44cc2478ea1bf2e5107d479da4a4a
TomBers <tjcberman@gmail.com>
2025-11-08 20:33:39 +0000

    take que concurrency back to 20

--- config/config.exs
+++ config/config.exs
@@ -10,7 +10,7 @@ import Config
 config :dialectic, Oban,
   engine: Oban.Engines.Basic,
   notifier: Oban.Notifiers.Postgres,
-  queues: [api_request: 10, openai_request: 50, openai_selection: 20, db_write: 5],
+  queues: [api_request: 10, openai_request: 20, openai_selection: 20, db_write: 5],
   repo: Dialectic.Repo,
   plugins: [
     {Oban.Plugins.Lifeline, rescue_after: 60}
