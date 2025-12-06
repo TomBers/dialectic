560b30916fd9aa4a4850b0285ed879f8c8fc2dc8
TomBers <tjcberman@gmail.com>
2025-12-06 18:46:06 +0000

    remove hardcoded questions

--- lib/dialectic_web/router.ex
+++ lib/dialectic_web/router.ex
@@ -35,7 +35,6 @@ defmodule DialecticWeb.Router do
     get "/intro/how", PageController, :guide
 
     get "/deploy/dashboard", PageController, :deploy_dashboard
-    get "/ideas/all", PageController, :ideas_all
 
     live "/inspiration", InspirationLive
     live "/start/new/idea", GraphLive
@@ -48,7 +47,6 @@ defmodule DialecticWeb.Router do
   scope "/api", DialecticWeb do
     pipe_through :api
 
-    get "/random_question", PageController, :random_question
     # get "/graphs/json/:graph_name", PageController, :graph_json
     get "/graphs/md/:graph_name", PageController, :graph_md
   end
