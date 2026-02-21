1e22605bea8bb78cb8cba90c1ced093467bca3eb
TomBers <tjcberman@gmail.com>
2026-02-21 10:32:24 +0000

    Fix my ideas not link bug (#244)
    * fix my ideas link bug
    
    * add share token
    
    * improve page performance
    
    * update wording
    
    * update ideas style
    
    * lighten icons
    
    * update toolbar
    
    * fix node count

--- lib/dialectic_web/graph_path_helper.ex
+++ lib/dialectic_web/graph_path_helper.ex
@@ -8,38 +8,62 @@ defmodule DialecticWeb.GraphPathHelper do
   @doc """
   Generates a path to a graph using its slug.
 
+  For private graphs with a share_token, the token is automatically
+  included as a query parameter so the link grants access.
+
   ## Examples
 
       # With a graph struct
-      graph_path(%{slug: "my-graph-abc123"})
+      graph_path(%{slug: "my-graph-abc123", is_public: true})
       # => "/g/my-graph-abc123"
 
       # With a node parameter
-      graph_path(%{slug: "my-graph-abc123"}, "5")
+      graph_path(%{slug: "my-graph-abc123", is_public: true}, "5")
       # => "/g/my-graph-abc123?node=5"
+
+      # Private graph with share token
+      graph_path(%{slug: "my-graph-abc123", is_public: false, share_token: "abc"})
+      # => "/g/my-graph-abc123?token=abc"
   """
   def graph_path(graph, node \\ nil, params \\ [])
 
-  def graph_path(%{slug: slug} = _graph, node, params) when not is_nil(slug) and slug != "" do
+  def graph_path(%{slug: slug} = graph, node, params) when not is_nil(slug) and slug != "" do
     base_path = "/g/#{slug}"
+    params = maybe_add_token(graph, params)
     build_path_with_params(base_path, node, params)
   end
 
   @doc """
   Generates a path to the linear view of a graph.
 
+  For private graphs with a share_token, the token is automatically
+  appended as a query parameter so the link grants access.
+
   ## Examples
 
-      graph_linear_path(%{slug: "my-graph-abc123"})
+      graph_linear_path(%{slug: "my-graph-abc123", is_public: true})
       # => "/g/my-graph-abc123/linear"
+
+      # Private graph with share token
+      graph_linear_path(%{slug: "my-graph-abc123", is_public: false, share_token: "abc"})
+      # => "/g/my-graph-abc123/linear?token=abc"
   """
   def graph_linear_path(graph, node_id \\ nil, params \\ [])
 
-  def graph_linear_path(%{slug: slug} = _graph, node_id, params)
+  def graph_linear_path(%{slug: slug} = graph, node_id, params)
       when not is_nil(slug) and slug != "" do
     base_path = "/g/#{slug}/linear"
+    params = maybe_add_token(graph, params)
     build_path_with_params(base_path, node_id && {:node_id, node_id}, params)
   end
+
+  # Appends the share_token as a "token" query param for private graphs
+  defp maybe_add_token(%{is_public: false, share_token: token}, params)
+       when is_binary(token) and token != "" do
+    [{"token", token} | params]
+  end
+
+  defp maybe_add_token(_graph, params), do: params
 
   # Private helper to build path with query parameters
   defp build_path_with_params(base_path, node_or_tuple, additional_params) do
