0e42e949809ba1ebd6f4c7184c7c14e750241cbc
TomBers <tjcberman@gmail.com>
2026-02-24 14:41:23 +0000

    make sure icons on one line

--- lib/dialectic_web/live/action_toolbar_comp.ex
+++ lib/dialectic_web/live/action_toolbar_comp.ex
@@ -85,7 +85,7 @@ defmodule DialecticWeb.ActionToolbarComp do
         class={
           if @inline,
             do:
-              "relative z-10 flex flex-wrap items-center justify-start gap-1 w-full pointer-events-auto",
+              "relative z-10 flex flex-nowrap items-center justify-start gap-1 w-full overflow-x-auto pointer-events-auto",
             else:
               "hidden sm:flex fixed left-1/2 -translate-x-1/2 z-10 bg-white shadow border border-gray-200 px-1.5 py-1 rounded-md items-center justify-center gap-1 pointer-events-auto max-w-[90vw]"
         }
