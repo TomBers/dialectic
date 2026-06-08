d02491df3d8042330172860ad28fbc76a73fac79
TomBers <tjcberman@gmail.com>
2026-06-08 09:12:17 +0100

    Improve migration

--- priv/repo/migrations/20260607133000_add_profile_media_links_and_remove_gravatar.exs
+++ priv/repo/migrations/20260607133000_add_profile_media_links_and_remove_gravatar.exs
@@ -1,24 +1,14 @@
 defmodule Dialectic.Repo.Migrations.AddProfileMediaLinksAndRemoveGravatar do
   use Ecto.Migration
 
-  def up do
-    execute("ALTER TABLE users ADD COLUMN IF NOT EXISTS avatar_path varchar(500)")
-    execute("ALTER TABLE users ADD COLUMN IF NOT EXISTS banner_path varchar(500)")
-    execute("ALTER TABLE users ADD COLUMN IF NOT EXISTS profile_banner varchar(100)")
+  def change do
+    alter table(:users) do
+      add :avatar_path, :text
+      add :banner_path, :text
+      add :profile_banner, :string
+      add :profile_links, :map, null: false, default: %{"links" => []}
 
-    execute("""
-    ALTER TABLE users
-    ADD COLUMN IF NOT EXISTS profile_links jsonb NOT NULL DEFAULT '{"links": []}'::jsonb
-    """)
-
-    execute("ALTER TABLE users DROP COLUMN IF EXISTS gravatar_id")
-  end
-
-  def down do
-    execute("ALTER TABLE users ADD COLUMN IF NOT EXISTS gravatar_id varchar(100)")
-    execute("ALTER TABLE users DROP COLUMN IF EXISTS profile_links")
-    execute("ALTER TABLE users DROP COLUMN IF EXISTS profile_banner")
-    execute("ALTER TABLE users DROP COLUMN IF EXISTS banner_path")
-    execute("ALTER TABLE users DROP COLUMN IF EXISTS avatar_path")
+      remove :gravatar_id
+    end
   end
 end
